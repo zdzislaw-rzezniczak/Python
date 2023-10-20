cdd11e707aff38da772c3eb4f319cd4c57d24f32
Zdzislaw <zdzichrz@gmail.com>
2023-10-20 15:54:20 +0200

    snake game part1

---
+++ snake_game/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self, ):
+        self.x_position = 0
+        self.segments = []
+
+
+    def create_snake(self):
+        for _ in range(0, 3):
+            turtle = Turtle("square")
+            turtle.penup()
+            turtle.color("white")
+            turtle.setposition(x=self.x_position, y=0)
+            self.x_position -= 20
+            self.segments.append(turtle)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.segments[0].heading() != DOWN:
+            self.segments[0].setheading(UP)
+
+    def down(self):
+        if self.segments[0].heading() != UP:
+            self.segments[0].setheading(DOWN)
+
+    def left(self):
+        if self.segments[0].heading() != RIGHT:
+            self.segments[0].setheading(LEFT)
+
+    def right(self):
+        if self.segments[0].heading() != LEFT:
+            self.segments[0].setheading(RIGHT)
