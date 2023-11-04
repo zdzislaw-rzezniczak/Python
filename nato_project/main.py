c40387623280d7553ab4e1743607dab9b6b73ad7
Zdzislaw <zdzichrz@gmail.com>
2023-11-04 11:02:19 +0100

    nato project exception handling

--- nato_project/main.py
+++ nato_project/main.py
@@ -4,8 +4,15 @@ nato_df = pd.read_csv('nato_phonetic_alphabet.csv')
 
 nato_dict = {d['letter']: d['code'] for d in nato_df.to_dict(orient='records')}
 
-users_word = input("podaj słowo \n").upper()
+is_correct = False
+while not is_correct:
+    try:
+        users_word = input("Podaj słowo \n").upper()
+        nato_word = [nato_dict[letter] for letter in users_word]
+        is_correct = True
+    except KeyError:
+        print("Niepoprawne słowo. Podaj poprawne słowo")
+        is_correct = False
 
-nato_word = [nato_dict[letter] for letter in users_word]
 print(nato_word)
 
