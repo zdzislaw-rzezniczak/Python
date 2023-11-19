c4015fc56915c563fcf5ab77a2e312b4929d24e3
Zdzislaw <zdzichrz@gmail.com>
2023-11-19 15:32:41 +0100

    spotify

---
+++ spotify_playlist/trojka.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy import SpotifyClientCredentials, SpotifyOAuth
+
+CLIENT_SPOTIFY_ID = "###########################"
+CLIENT_SPOTIFY_SECRET = "###########################"
+REDIRECT_URL = "http://example.com"
+
+
+url = "https://trojka.polskieradio.pl/notowania"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+songs = soup.select("h4.VotingList_songTitle__hY9pS")
+
+top_lista = [song.text.strip() for song in songs]
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_SPOTIFY_ID, client_secret=CLIENT_SPOTIFY_SECRET)
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri=REDIRECT_URL,
+        client_id=CLIENT_SPOTIFY_ID,
+        client_secret=CLIENT_SPOTIFY_SECRET,
+        cache_path="token.txt"
+    )
+)
+
+spotify_song_uris = []
+
+for value in top_lista:
+    spotify_result = sp.search(q=f"track:{value}", type="track")
+    try:
+        song_uri = spotify_result['tracks']['items'][0]['uri']
+        spotify_song_uris.append(song_uri)
+    except IndexError:
+        print(f"{value} Nie ma w Spotify. Opuszczony")
+
+
+user_id = sp.current_user()["id"]
+playlist = sp.user_playlist_create(user=user_id, name="Plejlista_TRÓJKA_2023", public=False)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=spotify_song_uris)
